12440f1c9a9b0021fe533d6a39b16f710949f6df
Fercho-93 <fernandosirventmerino@gmail.com>
2026-08-26 16:05:10 +0200

    Inicia la auditoría oficial de Fácil (data/official-corrections.js)

---
+++ data/official-corrections.js
@@ -0,0 +1,14 @@
+window.PIZARRA_OFFICIAL_CORRECTIONS = {
+  "Q96755": {
+    verifiedAt: "2026-08-26",
+    source: "https://www.realmadrid.com/es-ES/futbol/primer-equipo-masculino/plantilla/antonio-rudiger",
+    positions: ["Defensa"],
+    clubs: [
+      { id: "Q4512", name: "VfB Stuttgart", league: "bundesliga", leagueName: "Bundesliga", start: 2011, end: 2015 },
+      { id: "Q2739", name: "A.S. Roma", league: "seriea", leagueName: "Serie A", start: 2015, end: 2017 },
+      { id: "Q9616", name: "Chelsea F. C.", league: "premier", leagueName: "Premier League", start: 2017, end: 2022 },
+      { id: "Q8682", name: "Real Madrid Club de Fútbol", league: "laliga", leagueName: "LaLiga", start: 2022, end: null }
+    ]
+  }
+};
+
